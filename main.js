8e44f8704344ec8ca36463eca78fe8b27c04a866
Obiora Odimmegwa <48391689+ObioraOdimmegwa@users.noreply.github.com>
2022-07-13 19:01:44 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+LottieInteractivity.create({
+    mode:"scroll",
+    player: "#shoes",
+    container: "#lottieDiv",
+    actions: [
+        {
+            visibility:[0,1],
+            type: "play",
+            frames: [0, 60]
+        }
+    ]
+});
